466799bb6fdfc5634696d224b81b20add2987e57
Shivam49493 <shivamkmr203@gmail.com>
2025-09-09 10:26:48 +0530

    Remove backend from repository

--- backend/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addToCart, getUserCart, UpdateCart} from '../controllers/cartController.js';
-import isAuth from '../middleware/isAuth.js';
-
-const cartrouter = express.Router();
-cartrouter.post('/get', isAuth,getUserCart );
-cartrouter.post('/add', isAuth, addToCart);
-cartrouter.post('/update', isAuth,UpdateCart);
-export default cartrouter;
--- backend/routes/orderRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import isAuth from '../middleware/isAuth.js'
-import { placeOrder } from '../controllers/orderController.js'
-const orderRoutes= express.Router()
-orderRoutes.post('/placeorder',isAuth,placeOrder)
-export default orderRoutes
